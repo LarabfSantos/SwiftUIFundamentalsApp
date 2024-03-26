f462117d4c5c20de065459df849ce84de932fe2d
Lara Santos <lara.santos@madetech.com>
2024-03-26 14:02:42 +0000

    Initial Commit

---
+++ SwiftUIFundamentals/SwiftUIFundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFundamentalsApp.swift
+//  SwiftUIFundamentals
+//
+//  Created by Lara Santos on 26/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
