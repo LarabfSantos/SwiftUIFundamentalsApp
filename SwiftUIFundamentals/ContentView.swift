5838c18d64877d3f63d294b4006377adb7fc0626
Lara Santos <lara.santos@madetech.com>
2024-03-28 11:46:53 +0000

    Create list with images. Understand struct and value types

--- SwiftUIFundamentals/ContentView.swift
+++ SwiftUIFundamentals/ContentView.swift
@@ -8,17 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let hikes = [
+        Hike(name: "Photo of us", photo: "me-and-gabby", miles: 6.1),
+        Hike(name: "A girl hiking", photo: "hike", miles: 5)
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(hikes) { hike in
+            HStack {
+                Image(hike.photo)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(RoundedRectangle(cornerRadius: 12, style: .continuous))  //the order of where you chose the view modifiers matters. Showing this before the actual frame
+                    .frame(width: 100, height: 100)
+                Text(hike.name)
+            }
         }
         .padding()
     }
 }
 
-#Preview {
+#Preview { // preview macro to present the view created above
     ContentView()
 }
+
+// differences between Struct and Class
+//struct MyStruct {
+//    var value: Int
+//}
+//
+//class MyClass {
+//    var value: Int
+//
+//    init(value: Int) {
+//        self.value = value
+//    }
+//}
+//
+//var structInstance1 = MyStruct(value: 5)
+//var structInstance2 = structInstance1
+//structInstance2.value = 10
+//print(structInstance1.value) // Output: 5
+//
+//var classInstance1 = MyClass(value: 5)
+//var classInstance2 = classInstance1
+//classInstance2.value = 10
+//print(classInstance1.value) // Output: 10
+
---
+++ SwiftUIFundamentals/Models/Hike.swift
@@ -0,0 +1,15 @@
+//
+//  Hike.swift
+//  SwiftUIFundamentals
+//
+//  Created by Lara Santos on 27/03/2024.
+//
+
+import Foundation
+
+struct Hike: Identifiable {   //it needs to make sure the elements have an uniqueness to it - giving an identity to UI elements
+    let id = UUID()
+    let name: String
+    let photo: String
+    let miles: Double
+}
